ebd320dd8a78c57515393754dc4666e4b9159edc
Kelvin Lee <yckelvin@users.noreply.github.com>
2024-04-09 20:23:08 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.showNumber(0)
+ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+ESP8266_IoT.connectWifi("", "")
+basic.showNumber(1)
+let client_id = randint(0, 99999999)
+ESP8266_IoT.setMQTT(
+ESP8266_IoT.SchemeList.TCP,
+convertToText(client_id),
+"test",
+"test",
+""
+)
+ESP8266_IoT.connectMQTT("192.168.0.240", 1884, false)
+basic.showNumber(2)
+basic.showIcon(IconNames.Yes)
+loops.everyInterval(3600000, function () {
+	
+})
