a2b30ba66b5b60a9b01aa998f120578f3805d4a4
Kelvin Lee <yckelvin@users.noreply.github.com>
2024-04-24 14:28:21 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    ESP8266_IoT.publishMqttMessage("", "myhome/null/device_name", ESP8266_IoT.QosList.Qos2)
+    is_touch = pins.digitalReadPin(DigitalPin.P1)
+    if (is_touch == 1) {
+        OLED.clear()
+        OLED.writeStringNewLine("Sensor is touched")
+    } else {
+        OLED.clear()
+        OLED.writeStringNewLine("Sensor is not touched")
+    }
 })
+let is_touch = 0
 basic.showNumber(0)
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
 ESP8266_IoT.connectWifi("ssid", "password")
