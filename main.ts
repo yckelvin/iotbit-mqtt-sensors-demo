6f076a3ed5a8dc358259cb1ef08039bab1adca4e
Kelvin Lee <yckelvin@users.noreply.github.com>
2024-04-24 13:19:29 +0800

    restore default master

--- main.ts
+++ main.ts
@@ -1,11 +1,6 @@
 input.onButtonPressed(Button.A, function () {
-    light_level = pins.analogReadPin(AnalogPin.P1)
-    ESP8266_IoT.publishMqttMessage(convertToText(light_level), "myhome/null/light", ESP8266_IoT.QosList.Qos2)
-    OLED.clear()
-    OLED.writeString("Light: ")
-    OLED.writeNumNewLine(light_level)
+    ESP8266_IoT.publishMqttMessage("", "myhome/null/device_name", ESP8266_IoT.QosList.Qos2)
 })
-let light_level = 0
 basic.showNumber(0)
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
 ESP8266_IoT.connectWifi("ssid", "password")
