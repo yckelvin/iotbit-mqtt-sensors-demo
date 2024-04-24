9b3d2f20ca00deda38191d2f35b03a30fcf95e27
Kelvin Lee <yckelvin@users.noreply.github.com>
2024-04-24 13:13:44 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    ESP8266_IoT.publishMqttMessage("", "", ESP8266_IoT.QosList.Qos2)
+    light_level = pins.analogReadPin(AnalogPin.P1)
+    ESP8266_IoT.publishMqttMessage(convertToText(light_level), "myhome/null/light", ESP8266_IoT.QosList.Qos2)
+    OLED.clear()
+    OLED.writeString("Light: ")
+    OLED.writeNumNewLine(light_level)
 })
+let light_level = 0
 basic.showNumber(0)
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
-ESP8266_IoT.connectWifi("", "")
+ESP8266_IoT.connectWifi("ssid", "password")
 basic.showNumber(1)
 let client_id = randint(0, 99999999)
 ESP8266_IoT.setMQTT(
@@ -15,7 +20,9 @@ convertToText(client_id),
 )
 ESP8266_IoT.connectMQTT("192.168.0.240", 1884, false)
 basic.showNumber(2)
-basic.showIcon(IconNames.Yes)
+if (ESP8266_IoT.isMqttBrokerConnected()) {
+    basic.showIcon(IconNames.Yes)
+}
 loops.everyInterval(3600000, function () {
 	
 })
