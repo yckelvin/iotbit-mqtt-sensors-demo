644a609adff72d3ecbd26068d524257ac5f08f7a
Kelvin Lee <yckelvin@users.noreply.github.com>
2024-04-24 13:22:45 +0800

    add light reading

--- main.ts
+++ main.ts
@@ -1,13 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    rainfall = pins.analogReadPin(AnalogPin.P2)
-    ESP8266_IoT.publishMqttMessage(convertToText(rainfall), "myhome/null/rainfall", ESP8266_IoT.QosList.Qos2)
-    OLED.writeString("Rainfall: ")
-    OLED.writeNumNewLine(rainfall)
+    light2 = pins.analogReadPin(AnalogPin.P2)
+    ESP8266_IoT.publishMqttMessage(convertToText(light2), "myhome/null/light", ESP8266_IoT.QosList.Qos2)
+    OLED.clear()
+    OLED.writeString("Light: ")
+    OLED.writeNumNewLine(light2)
 })
-let rainfall = 0
+let light2 = 0
 basic.showNumber(0)
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
-ESP8266_IoT.connectWifi("", "")
+ESP8266_IoT.connectWifi("ssid", "password")
 basic.showNumber(1)
 let client_id = randint(0, 99999999)
 ESP8266_IoT.setMQTT(
@@ -20,7 +21,9 @@ convertToText(client_id),
 ESP8266_IoT.connectMQTT("192.168.0.240", 1884, false)
 basic.showNumber(2)
 OLED.init(128, 64)
-basic.showIcon(IconNames.Yes)
+if (ESP8266_IoT.isMqttBrokerConnected()) {
+    basic.showIcon(IconNames.Yes)
+}
 loops.everyInterval(3600000, function () {
 	
 })
